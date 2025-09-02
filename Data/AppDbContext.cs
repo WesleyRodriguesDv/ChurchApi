08a20f56761bcecd276b88ec917b59f24b6d92ca
WesleyRodriguesDv <wesley.rodriiguesdev@gmail.com>
2025-09-02 09:05:32 -0300

    database: DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ChurchApi.Data;
+﻿using ChurchApi.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace ChurchApi.Data;
+
+public class AppDbContext:DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<DepartamentModel> Departaments { get; set; }
+    public DbSet<MemberModel> Members { get; set; }
 }
