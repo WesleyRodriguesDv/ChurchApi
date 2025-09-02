ca93739b74b9750d9c5472416cf8f8c6966468da
WesleyRodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-09-02 09:54:25 -0300

    feat: Many-to-many relationship between Members and Departments

--- Models/MemberModel.cs
+++ Models/MemberModel.cs
@@ -5,5 +5,5 @@ public class MemberModel
     public int Id { get; set; }
     public string Name { get; set; }
     public string LastName { get; set; }
-    public DepartamentModel Departament { get; set; }
+    public ICollection<DepartamentModel> Departaments { get; set; }
 }
