819e9a83486500ea780c9f10fa69728aedeeacf8
WesleyRodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-09-03 08:48:01 -0300

    feat: add IDepartamentInterface

---
+++ Interfaces/IDepartamentInterface.cs
@@ -0,0 +1,10 @@
+﻿using ChurchApi.Models;
+
+namespace ChurchApi.Interfaces;
+
+public interface IDepartamentInterface
+{
+    Task<ResponseModel<List<DepartamentModel>>> GetDepartaments();
+    Task<ResponseModel<List<DepartamentModel>>> GetDepartamentsForIdMember(int MemberId);
+    Task<ResponseModel<DepartamentModel>> GetDepartamtsForId(int DepartamentId); 
+}
