1975e3f5349c7b9731986246b648135ebcf53c7f
WesleyRodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-09-03 09:06:20 -0300

    feat: Dependency Injection between Services and Interfaces

--- Services/Departament/DepartamentService.cs
+++ Services/Departament/DepartamentService.cs
@@ -1,5 +1,51 @@
-﻿namespace ChurchApi.Services;
+﻿using Azure;
+using ChurchApi.Data;
+using ChurchApi.Interfaces;
+using ChurchApi.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class DepartamentService
+namespace ChurchApi.Services;
+
+public class DepartamentService:IDepartamentInterface
 {
+    private readonly AppDbContext _context;
+
+    public DepartamentService(AppDbContext context)
+    {
+        _context = context;
     }
+    
+    public async Task<ResponseModel<List<DepartamentModel>>> GetDepartaments()
+    {
+        ResponseModel<List<DepartamentModel>> response = new ResponseModel<List<DepartamentModel>>();
+        try
+        {
+            var departaments = await _context.Departaments.ToListAsync();
+            response.Data = departaments;
+            response.Message = "Todos os Departamentos foram listados com sucesso!";
+            return response;
+        }
+        catch (Exception e)
+        {
+            response.Message = e.Message;
+            response.Status = false;
+            return response;
+        }    
+        
+    }
+
+    
+    
+    
+    
+    
+    public Task<ResponseModel<List<DepartamentModel>>> GetDepartamentsForIdMember(int MemberId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseModel<DepartamentModel>> GetDepartamtsForId(int DepartamentId)
+    {
+        throw new NotImplementedException();
+    }
+}
