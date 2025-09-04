f7c4c197db78c50808474689168f2ccfd636304e
WesleyRodriguesDv <wesley.rodriiguesdev@gmail.com>
2025-09-04 10:11:12 -0300

    feat: rename variables and remove Task

--- Interfaces/IDepartamentInterface.cs
+++ Interfaces/IDepartamentInterface.cs
@@ -5,6 +5,5 @@ namespace ChurchApi.Interfaces;
 public interface IDepartamentInterface
 {
     Task<ResponseModel<List<DepartamentModel>>> GetDepartaments();
-    Task<ResponseModel<List<DepartamentModel>>> GetDepartamentsForIdMember(int MemberId);
-    Task<ResponseModel<DepartamentModel>> GetDepartamtsForId(int DepartamentId); 
+    Task<ResponseModel<DepartamentModel>> GetDepartamentById(int departamentId); 
 }
