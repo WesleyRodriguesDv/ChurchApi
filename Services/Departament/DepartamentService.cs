9b2c9af4be45afd5652aacf2fabff848516d3084
WesleyRodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-09-11 09:56:12 -0300

    feat: add services delete, edit in departaments

--- Services/Departament/DepartamentService.cs
+++ Services/Departament/DepartamentService.cs
@@ -4,6 +4,7 @@ using ChurchApi.DTOs.Departament;
 using ChurchApi.Interfaces;
 using ChurchApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ChurchApi.Services.Departament;
 
@@ -81,5 +82,58 @@ public class DepartamentService:IDepartamentInterface
             response.Status = false;
             return response;
         }            
-    }                        
+    }
+    
+    public async Task<ResponseModel<DepartamentModel>> EditDepartament(DepartamentEditDTO departamentEditDto)
+    {
+        ResponseModel<DepartamentModel> response = new ResponseModel<DepartamentModel>();
+        try
+        {
+            var departament = await _context.Departaments.FirstOrDefaultAsync(x => x.Id == departamentEditDto.Id);
+            if (departament == null)
+            {
+                response.Message = "Departamento não encontrado!";
+                response.Status = false;
+            }
+
+            departament.Name = departamentEditDto.Name;
+            _context.Update(departament);
+            await _context.SaveChangesAsync();
+
+            response.Message = "Departamento alterado com sucesso";
+            return response;
+        }
+        catch (Exception e)
+        {
+            response.Message = e.Message;
+            response.Status = false;
+            return response;
+        }
+    }
+
+    public async Task<ResponseModel<DepartamentModel>> DeleteDepartament(int departamentId)
+    {
+        ResponseModel<DepartamentModel> response = new ResponseModel<DepartamentModel>();
+        try
+        {
+            var departament = await _context.Departaments.FirstOrDefaultAsync(x => x.Id == departamentId);
+            if (departament == null)
+            {
+                response.Message = "Departamento não encontrado";
+                response.Status = false;
+            }
+
+            _context.Remove(departament);
+            await _context.SaveChangesAsync();
+            response.Message = "Departamento deletado com sucesso!";
+            return response;
+
+        }
+        catch (Exception e)
+        {
+            response.Message = e.Message;
+            response.Status = false;
+            return response;
+        }
+    }
 }                                       
