badc921bf1fd42ae3490e7a4098f2bd7b2234b3f
WesleyRodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-09-11 10:52:20 -0300

    feat: routes Get/Members

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,33 @@
+using ChurchApi.Interfaces;
+using ChurchApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChurchApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly IMemberInterface _memberInterface;
+
+        public MemberController(IMemberInterface memberInterface)
+        {
+            _memberInterface = memberInterface;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ResponseModel<List<MemberModel>>>> GetAll()
+        {
+            var members = await _memberInterface.GetMembers();
+            return Ok(members);
+        }
+
+        [HttpGet("GetBy/{memberId}")]
+        public async Task<ActionResult<ResponseModel<MemberModel>>> GetMemberById(int memberId)
+        {
+            var member = await _memberInterface.GetMemberById(memberId);
+            return Ok(member);
+        }
+    }
+}
