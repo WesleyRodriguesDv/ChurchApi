d549abcf1294ef7b78211378e6d4761b8981f792
WesleyRodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-09-15 11:09:51 -0300

    chore: migrations update

--- Migrations/20250913151732_2.1.Designer.cs
+++ Migrations/20250913151732_2.1.Designer.cs
@@ -3,6 +3,7 @@ using ChurchApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChurchApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250913151732_2.1")]
+    partial class _21
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250913151732_2.1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChurchApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class _21 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_Departaments_DepartamentIdId",
+                table: "Members");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartamentIdId",
+                table: "Members",
+                newName: "DepartamentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Members_DepartamentIdId",
+                table: "Members",
+                newName: "IX_Members_DepartamentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_Departaments_DepartamentId",
+                table: "Members",
+                column: "DepartamentId",
+                principalTable: "Departaments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_Departaments_DepartamentId",
+                table: "Members");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartamentId",
+                table: "Members",
+                newName: "DepartamentIdId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Members_DepartamentId",
+                table: "Members",
+                newName: "IX_Members_DepartamentIdId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_Departaments_DepartamentIdId",
+                table: "Members",
+                column: "DepartamentIdId",
+                principalTable: "Departaments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
